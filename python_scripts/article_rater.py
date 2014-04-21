ba98a4aa9fae0d5619d6868110fc737ea42e427c
Andrew Becker <adb2jb@virginia.edu>
2014-04-21 17:48:43 -0400

    article_rater deleted

--- python_scripts/article_rater.py
+++
@@ -1,9 +0,0 @@
-def get_raw_article_rating(time, hit_bottom, num_taps, words):
-    score = 0
-    words_per_minute = 250
-    if hit_bottom:
-        score = score * 1.2  # arbitrary!!
-    amount_read = words_per_minute * time / words
-    score += amount_read
-    score += num_taps
-    return score
